79fcbe26f20751a255908dc143cfce2d5cc8ff3a
shinnthant2004 <shinthantequi@gmail.com>
2024-07-22 09:34:23 +0630

    feat(app): implement board action component with copy clipboard & delete

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -8,6 +8,8 @@ import { useAuth } from "@clerk/nextjs";
 import { formatDistanceToNow } from "date-fns";
 import Footer from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
+import Actions from "@/components/actions";
+import { MoreHorizontal } from "lucide-react";
 
 interface BoardCardProps {
   id: string;
@@ -40,6 +42,11 @@ export default function BoardCard({
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
         <div className="relative flex-1 bg-amber-50">
           <Image src={imageUrl} fill alt={title} className="object-fill" />
+          <Actions id={id} title={title} side="right">
+            <button className="absolute z-[1] top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-2 py-2 outline-none">
+              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
+            </button>
+          </Actions>
           <Overlay />
         </div>
 
---
+++ components/actions.tsx
@@ -0,0 +1,62 @@
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Link2, Trash2 } from "lucide-react";
+import { toast } from "sonner";
+import { useApiMutation } from "@/hooks/use-api.mutation";
+import { api } from "@/convex/_generated/api";
+
+interface ActionProps {
+  children: React.ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  id: string;
+  title: string;
+}
+
+export default function Actions({
+  children,
+  side,
+  sideOffset,
+  id,
+  title,
+}: ActionProps) {
+  const { mutate, pending } = useApiMutation(api.board.remove);
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => toast.success("Link copied"))
+      .catch(() => toast.error("Failed to copy link"));
+  };
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => toast.success("Board deleted"))
+      .catch(() => toast.error("Failed to delete board"));
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+      >
+        <DropdownMenuItem onClick={onCopyLink} className="p-3 cursor-pointer">
+          <Link2 className="w-4 h-4 mr-2" /> Copy board link
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={onDelete} className="p-3 cursor-pointer">
+          <Trash2 className="w-4 h-4 mr-2" /> Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
