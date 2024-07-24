8253780fb10084877db9a0be11c200b4cff37a39
shinnthant2004 <shinthantequi@gmail.com>
2024-07-24 11:10:17 +0630

    feat(app): implement convas state

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,20 +1,37 @@
 "use client";
 
-import { useSelf } from "@liveblocks/react";
+import { useCanRedo, useCanUndo, useHistory } from "@liveblocks/react";
 import Info from "./info";
 import Participants from "./participants";
 import Toolbar from "./toolbar";
+import { useState } from "react";
+import { CanvasMode, CanvasState } from "@/types/canvas";
 
 interface CanvasProps {
   boardId: string;
 }
 
 export default function Canvas({ boardId }: CanvasProps) {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canRedo = useCanRedo();
+  const canUndo = useCanUndo();
+
   return (
     <main className="w-full h-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        redo={history.redo}
+        undo={history.undo}
+      />
     </main>
   );
 }
