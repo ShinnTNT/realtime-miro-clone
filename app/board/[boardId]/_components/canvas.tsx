e45c86aac1039df7e045f5c8e0fa969136095ff7
shinnthant2004 <shinthantequi@gmail.com>
2024-07-26 10:25:42 +0630

    feat(app): implement selection box

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -26,6 +26,7 @@ import { CursorsPresence } from "./cursors-presence";
 import { connectionIdToColor, pointerEventToCanvasPoint } from "@/lib/utils";
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
+import { SelectionBox } from "./selection-box";
 
 const MAX_LAYERS = 100;
 
@@ -190,6 +191,7 @@ export default function Canvas({ boardId }: CanvasProps) {
             />
           ))}
           <CursorsPresence />
+          <SelectionBox onResizeHandlePointerDown={() => {}} />
         </g>
       </svg>
     </main>
